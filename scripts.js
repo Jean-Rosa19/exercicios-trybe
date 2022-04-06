33b47a6f84d07ca471101246353377d09f960920
Jean Rosa <jeancarlosrosa22@gmail.com>
2022-04-06 19:23:10 -0300

    exercicios em JavaScript

---
+++ scripts.js
@@ -0,0 +1,30 @@
+let num1 = 19
+let num2 = 7
+console.log (num1 + num2)
+console.log (num1 - num2)
+console.log (num1 * num2)
+console.log (num1 / num2)
+console.log (num1 % num2)
+
+let m1 = 15;
+let m2 = 8;
+if (m1 > m2){
+    console.log("é maior")
+} else {
+    console.log("é menor")
+}
+
+var number1 = 14;
+var number2 = 27;
+var number3 = 35;
+
+if (number1 > number2) {
+    console.log ("é maior")
+} else if(number2 > number3) {
+    console.log("number2 é maior")
+} else {
+    console.log("number3 é o maior")
+}
+
+
+
