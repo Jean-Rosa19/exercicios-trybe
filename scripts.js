a7250143b6f4d0be299f89b0e1099e658e5d0347
Jean Rosa <jeancarlosrosa22@gmail.com>
2022-04-07 09:49:14 -0300

    novas resoluções exercicio 4.1

--- scripts.js
+++ scripts.js
@@ -26,5 +26,44 @@ if (number1 > number2) {
     console.log("number3 é o maior")
 }
 
+// 4 - exercicio para retornar se é positivo ou falso
+let pos1 = -13
+if (-13 > 0) {
+    console.log("positivo")
+} else {
+    console.log ("negativo")
+}
+
+// 5 - exercicio angulos do triangulo
+let ang1 = 30
+let ang2 = 70
+let ang3 = 80
+if (ang1+ ang2+ ang3 === 180){
+    console.log ("true")
+} else (ang1+ang2+ang3 != 180);{
+    console.log ("false")
+}
+
+// programa peças de zadrez
+let peça = "torre"
+    switch(peça){
+        case "rei": 
+        console.log("rei => anda uma casa em qualquer direção");
+        break;
+        
+        case "bispo": 
+        console.log ("bispo => anda em diagonal");
+        break;
+
+        case "torre": 
+        console.log("torre => anda em linha reta")
+        break
+    
+    }
+
+        
+   
+    
+
 
 
